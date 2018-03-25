a66bd66bd3bc9a08d67b76d6d7aa85592293a8e0
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-25 15:37:53 -0400

    Rename day01 crates

--- day01-part-1/examples/day01-part-1.rs
+++ day01-part-1/examples/day01-part-1.rs
@@ -1,9 +1,9 @@
-extern crate day01;
+extern crate day01_part_1;
 
 const PUZZLE_INPUT: &'static str = include_str!("../day01_input.txt");
 
 fn main() {
-    let solution = day01::aoc_day01(PUZZLE_INPUT);
+    let solution = day01_part_1::aoc_day01(PUZZLE_INPUT);
     println!("PUZZLE_INPUT: {}", PUZZLE_INPUT);
     println!("solution: {}", solution);
 }
