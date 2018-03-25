fff1dddfb2261daa9a89cad4f37b678bff0b2d8e
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-25 14:54:24 -0400

    Add day 01 implementation and tests

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,7 +1,62 @@
+pub fn aoc_day01(input: &str) -> u32 {
+    let mut count = input.chars().skip(1).zip(input.chars()).fold(0, |acc, (u, l)| {
+        if u == l {
+            acc + l.to_digit(10).unwrap()
+        } else {
+            acc
+        }
+    });
+
+    // Don't forget to check last and first
+    if input.chars().nth(0).unwrap() == input.chars().last().unwrap() {
+        count += input.chars().nth(0).unwrap().to_digit(10).unwrap();
+    }
+
+    count
+}
+
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    mod aoc2017 {
+        mod day01 {
+            use ::*;
+
+            #[test]
+            fn example_01_1122() {
+                let input = "1122";
+                let expected = 3;
+                let to_check = aoc_day01(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn example_02_1111() {
+                let input = "1111";
+                let expected = 4;
+                let to_check = aoc_day01(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn example_03_1234() {
+                let input = "1234";
+                let expected = 0;
+                let to_check = aoc_day01(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn example_04_91212129() {
+                let input = "91212129";
+                let expected = 9;
+                let to_check = aoc_day01(input);
+
+                assert_eq!(expected, to_check);
+            }
+        }
     }
 }
