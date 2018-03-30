c432ed873bd1197c20e70d3739b63c5fe261d8d6
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-30 14:44:40 -0400

    Move parsed vector into constant

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -35,19 +35,6 @@ mod tests {
         mod day07 {
             use ::*;
 
-            const EXAMPLE1: &str = "pbga (66)
-                                    xhth (57)
-                                    ebii (61)
-                                    havc (66)
-                                    ktlj (57)
-                                    fwft (72) -> ktlj, cntj, xhth
-                                    qoyq (66)
-                                    padx (45) -> pbga, havc, qoyq
-                                    tknk (41) -> ugml, padx, fwft
-                                    jptl (61)
-                                    ugml (68) -> gyxo, ebii, jptl
-                                    gyxo (61)
-                                    cntj (57)";
             /*
                           gyxo
                         /
@@ -67,65 +54,78 @@ mod tests {
                         \
                             xhth
             */
+            const EXAMPLE1: &str = "pbga (66)
+                                    xhth (57)
+                                    ebii (61)
+                                    havc (66)
+                                    ktlj (57)
+                                    fwft (72) -> ktlj, cntj, xhth
+                                    qoyq (66)
+                                    padx (45) -> pbga, havc, qoyq
+                                    tknk (41) -> ugml, padx, fwft
+                                    jptl (61)
+                                    ugml (68) -> gyxo, ebii, jptl
+                                    gyxo (61)
+                                    cntj (57)";
+            const EXAMPLE1_PROGRAMS: [Program; 13] = [
+                Program {
+                    name: "pbga",
+                    nb_disks: 66,
+                },
+                Program {
+                    name: "xhth",
+                    nb_disks: 57,
+                },
+                Program {
+                    name: "ebii",
+                    nb_disks: 61,
+                },
+                Program {
+                    name: "havc",
+                    nb_disks: 66,
+                },
+                Program {
+                    name: "ktlj",
+                    nb_disks: 57,
+                },
+                Program {
+                    name: "fwft",
+                    nb_disks: 72,
+                },
+                Program {
+                    name: "qoyq",
+                    nb_disks: 66,
+                },
+                Program {
+                    name: "padx",
+                    nb_disks: 45,
+                },
+                Program {
+                    name: "tknk",
+                    nb_disks: 41,
+                },
+                Program {
+                    name: "jptl",
+                    nb_disks: 61,
+                },
+                Program {
+                    name: "ugml",
+                    nb_disks: 68,
+                },
+                Program {
+                    name: "gyxo",
+                    nb_disks: 61,
+                },
+                Program {
+                    name: "cntj",
+                    nb_disks: 57,
+                },
+            ];
 
             #[test]
             fn example_01_parse_input() {
                 let to_check = parse_input(EXAMPLE1);
-                let expected = vec![
-                    Program {
-                        name: "pbga",
-                        nb_disks: 66,
-                    },
-                    Program {
-                        name: "xhth",
-                        nb_disks: 57,
-                    },
-                    Program {
-                        name: "ebii",
-                        nb_disks: 61,
-                    },
-                    Program {
-                        name: "havc",
-                        nb_disks: 66,
-                    },
-                    Program {
-                        name: "ktlj",
-                        nb_disks: 57,
-                    },
-                    Program {
-                        name: "fwft",
-                        nb_disks: 72,
-                    },
-                    Program {
-                        name: "qoyq",
-                        nb_disks: 66,
-                    },
-                    Program {
-                        name: "padx",
-                        nb_disks: 45,
-                    },
-                    Program {
-                        name: "tknk",
-                        nb_disks: 41,
-                    },
-                    Program {
-                        name: "jptl",
-                        nb_disks: 61,
-                    },
-                    Program {
-                        name: "ugml",
-                        nb_disks: 68,
-                    },
-                    Program {
-                        name: "gyxo",
-                        nb_disks: 61,
-                    },
-                    Program {
-                        name: "cntj",
-                        nb_disks: 57,
-                    },
-                ];
-                assert_eq!(expected, to_check);
+                assert_eq!(to_check, EXAMPLE1_PROGRAMS);
             }
 
             #[test]
