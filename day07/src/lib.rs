f79c570a76795e2e874d409eece1e1ccd3044870
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-30 14:04:24 -0400

    Add day07 template

---
+++ day07/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn aoc_day07(instructions: &str) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    mod aoc2017 {
+        mod day07 {
+            use ::*;
+
+            #[test]
+            fn example_01() {}
+
+            #[test]
+            fn solution() {}
+        }
+    }
+}
