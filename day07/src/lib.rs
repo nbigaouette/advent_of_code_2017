2d161577afb8eecb706a59675f243187918c33fa
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-30 14:38:02 -0400

    Initial parsing of the programs

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,4 +1,31 @@
-pub fn aoc_day07(instructions: &str) -> u32 {
+#[derive(Debug, PartialEq)]
+struct Program<'a> {
+    name: &'a str,
+    nb_disks: u32,
+}
+
+fn parse_input<'a>(input: &'a str) -> Vec<Program<'a>> {
+    input
+        .lines()
+        .map(|line| {
+            let mut word_iter = line.split_whitespace();
+            let program_name = word_iter.next().unwrap();
+            let nb_disks: u32 = word_iter
+                .next()
+                .unwrap()
+                .trim_matches('(')
+                .trim_matches(')')
+                .parse()
+                .unwrap();
+            Program {
+                name: program_name,
+                nb_disks: nb_disks,
+            }
+        })
+        .collect()
+}
+
+pub fn aoc_day07(input: &str) -> String {
     unimplemented!()
 }
 
@@ -42,10 +69,81 @@ mod tests {
             */
 
             #[test]
-            fn example_01() {}
+            fn example_01_parse_input() {
+                let to_check = parse_input(EXAMPLE1);
+                let expected = vec![
+                    Program {
+                        name: "pbga",
+                        nb_disks: 66,
+                    },
+                    Program {
+                        name: "xhth",
+                        nb_disks: 57,
+                    },
+                    Program {
+                        name: "ebii",
+                        nb_disks: 61,
+                    },
+                    Program {
+                        name: "havc",
+                        nb_disks: 66,
+                    },
+                    Program {
+                        name: "ktlj",
+                        nb_disks: 57,
+                    },
+                    Program {
+                        name: "fwft",
+                        nb_disks: 72,
+                    },
+                    Program {
+                        name: "qoyq",
+                        nb_disks: 66,
+                    },
+                    Program {
+                        name: "padx",
+                        nb_disks: 45,
+                    },
+                    Program {
+                        name: "tknk",
+                        nb_disks: 41,
+                    },
+                    Program {
+                        name: "jptl",
+                        nb_disks: 61,
+                    },
+                    Program {
+                        name: "ugml",
+                        nb_disks: 68,
+                    },
+                    Program {
+                        name: "gyxo",
+                        nb_disks: 61,
+                    },
+                    Program {
+                        name: "cntj",
+                        nb_disks: 57,
+                    },
+                ];
+                assert_eq!(expected, to_check);
+            }
 
             #[test]
-            fn solution() {}
+            fn example_01() {
+                let expected = "tknk";
+                let to_check = aoc_day07(EXAMPLE1);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn solution() {
+                // const PUZZLE_INPUT: &'static str = include_str!("../day07_input.txt");
+                // let expected = ???;
+                // let to_check = aoc_day07(PUZZLE_INPUT);
+
+                // assert_eq!(expected, to_check);
+            }
         }
     }
 }
