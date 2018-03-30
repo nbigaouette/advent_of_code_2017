68938481c60e57925c520f528f5877ef223ba926
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-29 20:21:02 -0400

    Add day06 template

---
+++ day06/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn aoc_day06(instructions: &str) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    mod aoc2017 {
+        mod day06 {
+            use ::*;
+
+            #[test]
+            fn example_01() {}
+
+            #[test]
+            fn solution() {}
+        }
+    }
+}
