0e5f0c24b7ad4fe9c4ac8f673b36a113a9277e26
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-30 14:37:46 -0400

    Add example

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -8,6 +8,39 @@ mod tests {
         mod day07 {
             use ::*;
 
+            const EXAMPLE1: &str = "pbga (66)
+                                    xhth (57)
+                                    ebii (61)
+                                    havc (66)
+                                    ktlj (57)
+                                    fwft (72) -> ktlj, cntj, xhth
+                                    qoyq (66)
+                                    padx (45) -> pbga, havc, qoyq
+                                    tknk (41) -> ugml, padx, fwft
+                                    jptl (61)
+                                    ugml (68) -> gyxo, ebii, jptl
+                                    gyxo (61)
+                                    cntj (57)";
+            /*
+                          gyxo
+                        /
+                    ugml - ebii
+                /      \
+                |         jptl
+                |
+                |         pbga
+                /        /
+            tknk --- padx - havc
+                \        \
+                |         qoyq
+                |
+                |         ktlj
+                \      /
+                    fwft - cntj
+                        \
+                            xhth
+            */
+
             #[test]
             fn example_01() {}
 
