3d8b7e993b4b6c532a9ca77d67c36d73c84d59c5
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-30 21:35:53 -0400

    Fix day02-part-2 function names

--- day02-part-2/src/lib.rs
+++ day02-part-2/src/lib.rs
@@ -1,11 +1,11 @@
-pub fn aoc_day02-part-2(instructions: &str) -> u32 {
+pub fn aoc_day02_part_2(instructions: &str) -> u32 {
     unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
     mod aoc2017 {
-        mod day02-part-2 {
+        mod day02_part_2 {
             use ::*;
 
             #[test]
