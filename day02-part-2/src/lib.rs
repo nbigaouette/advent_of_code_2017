231ecb98c4566d1cf5499a022b33da4d460939e6
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-03-30 21:33:59 -0400

    Add day02-part-2 template

---
+++ day02-part-2/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn aoc_day02-part-2(instructions: &str) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    mod aoc2017 {
+        mod day02-part-2 {
+            use ::*;
+
+            #[test]
+            fn example_01() {}
+
+            #[test]
+            fn solution() {}
+        }
+    }
+}
