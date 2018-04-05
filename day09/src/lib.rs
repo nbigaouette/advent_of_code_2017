bd15d3872d60b24dc7c4507a36b3981165fc298b
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-04-04 20:09:59 -0400

    Add new day from template

---
+++ day09/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn aoc_day09(instructions: &str) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    mod aoc2017 {
+        mod day09 {
+            use ::*;
+
+            // const PUZZLE_INPUT: &'static str = include_str!("../input");
+
+            #[test]
+            fn example_01() {}
+
+            #[test]
+            fn solution() {
+                // let expected = ???;
+                // let input = ???;
+                // let to_check = aoc_day09(PUZZLE_INPUT);
+
+                // assert_eq!(expected, to_check);
+            }
+        }
+    }
+}
