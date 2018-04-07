ee529ed2057704c23016ce827c3db2c821f9b3cf
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-04-07 16:58:02 -0400

    Add solution to day09 part 2

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,9 +1,10 @@
-pub fn aoc_day09(input: &str) -> (Vec<(usize, usize)>, usize) {
-    let mut bracket_stack_pos = Vec::<usize>::new();
-    let mut group_spans = Vec::<(usize, usize)>::new();
+pub fn aoc_day09(input: &str) -> (Vec<(usize, usize)>, usize, usize) {
+    let mut stack_start_group = Vec::<usize>::new();
+    let mut spans_group = Vec::<(usize, usize)>::new();
     let mut total_score = 0;
     let mut nested_level = 0;
 
+    let mut nb_garbage_chars = 0;
     let mut is_garbage = false;
     let mut next_is_ignored = false;
     let mut it = input.chars().enumerate();
@@ -13,29 +14,39 @@ pub fn aoc_day09(input: &str) -> (Vec<(usize, usize)>, usize) {
         } else {
             match c {
                 '!' => next_is_ignored = true,
-                _ => match c {
-                    '<' => is_garbage = true,
-                    '>' => is_garbage = false,
-                    '{' => {
-                        if !is_garbage {
-                            bracket_stack_pos.push(i);
-                            nested_level += 1;
-                        }
+                _ => {
+                    if is_garbage {
+                        nb_garbage_chars += 1;
                     }
-                    '}' => {
-                        if !is_garbage {
-                            group_spans.push((bracket_stack_pos.pop().unwrap(), i));
-                            total_score += nested_level;
-                            nested_level -= 1;
+                    match c {
+                        '<' => {
+                            is_garbage = true;
                         }
+                        '>' => {
+                            is_garbage = false;
+                            nb_garbage_chars -= 1;
+                        }
+                        '{' => {
+                            if !is_garbage {
+                                stack_start_group.push(i);
+                                nested_level += 1;
+                            }
+                        }
+                        '}' => {
+                            if !is_garbage {
+                                spans_group.push((stack_start_group.pop().unwrap(), i));
+                                total_score += nested_level;
+                                nested_level -= 1;
+                            }
+                        }
+                        _ => {}
                     }
-                    _ => {}
-                },
+                }
             }
         }
     }
 
-    (group_spans, total_score)
+    (spans_group, total_score, nb_garbage_chars)
 }
 
 #[cfg(test)]
@@ -74,13 +85,23 @@ mod tests {
                 ("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3),
             ];
 
+            const EXAMPLE_O4_GARBAGE: [(&str, usize); 7] = [
+                ("<>", 0),
+                ("<random characters>", 17),
+                ("<<<<>", 3),
+                ("<{!>}>", 2),
+                ("<!!>", 0),
+                ("<!!!>>", 0),
+                (r#"<{o"i!a,<{i<a>"#, 10),
+            ];
+
             const PUZZLE_INPUT: &'static str = include_str!("../input");
 
             #[test]
             fn example_01_count() {
                 for &(input, expected_count) in EXAMPLE_O1_COUNT.iter() {
-                    let (group_spans, _) = aoc_day09(input);
-                    let to_check_count = group_spans.len();
+                    let (spans_group, _, _) = aoc_day09(input);
+                    let to_check_count = spans_group.len();
                     assert_eq!(expected_count, to_check_count);
                 }
             }
@@ -88,8 +109,8 @@ mod tests {
             #[test]
             fn example_extras_count() {
                 for &(input, expected_count) in EXAMPLE_EXTRAS_COUNT.iter() {
-                    let (group_spans, _) = aoc_day09(input);
-                    let to_check_count = group_spans.len();
+                    let (spans_group, _, _) = aoc_day09(input);
+                    let to_check_count = spans_group.len();
                     assert_eq!(expected_count, to_check_count);
                 }
             }
@@ -97,15 +118,31 @@ mod tests {
             #[test]
             fn example_01_score() {
                 for &(input, expected_score) in EXAMPLE_O1_SCORE.iter() {
-                    let (_, to_check_score) = aoc_day09(input);
+                    let (_, to_check_score, _) = aoc_day09(input);
                     assert_eq!(expected_score, to_check_score);
+                }
+            }
+
+            #[test]
+            fn example_04_garbage_count() {
+                for &(input, expected) in EXAMPLE_O4_GARBAGE.iter() {
+                    let (_, _, to_check) = aoc_day09(input);
+                    assert_eq!(expected, to_check);
                 }
             }
 
             #[test]
             fn solution_part_1() {
                 let expected = 12897;
-                let (_, to_check) = aoc_day09(PUZZLE_INPUT);
+                let (_, to_check, _) = aoc_day09(PUZZLE_INPUT);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn solution_part_2() {
+                let expected = 7031;
+                let (_, _, to_check) = aoc_day09(PUZZLE_INPUT);
 
                 assert_eq!(expected, to_check);
             }
