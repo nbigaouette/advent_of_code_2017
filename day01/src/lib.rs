c6df6e8a14231993d6c2b9829045e04d1edfa1e4
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-04-09 19:49:10 -0400

    Merge `day01-part-1` and `day01-part-2`

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -39,11 +39,11 @@
 //!
 //! _What is the solution_ to your new captcha?
 
-pub fn aoc_day01_part_two(input: &str) -> u32 {
+pub fn aoc_day01(input: &str, skip: usize) -> u32 {
     input
         .chars()
         .cycle()
-        .skip(input.len() / 2)
+        .skip(skip)
         .zip(input.chars())
         .fold(0, |acc, (u, l)| {
             if u == l {
@@ -54,62 +54,115 @@ pub fn aoc_day01_part_two(input: &str) -> u32 {
         })
 }
 
+pub fn aoc_day01_part_1(input: &str) -> u32 {
+    aoc_day01(input, 1)
+}
+
+pub fn aoc_day01_part_2(input: &str) -> u32 {
+    aoc_day01(input, input.len() / 2)
+}
+
 #[cfg(test)]
 mod tests {
     mod aoc2017 {
-        mod day01_part_two {
+        mod day01 {
             use ::*;
 
             #[test]
-            fn example_01_1212() {
+            fn part_1_example_01_1122() {
+                let input = "1122";
+                let expected = 3;
+                let to_check = aoc_day01_part_1(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn part_1_example_02_1111() {
+                let input = "1111";
+                let expected = 4;
+                let to_check = aoc_day01_part_1(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn part_1_example_03_1234() {
+                let input = "1234";
+                let expected = 0;
+                let to_check = aoc_day01_part_1(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn part_1_example_04_91212129() {
+                let input = "91212129";
+                let expected = 9;
+                let to_check = aoc_day01_part_1(input);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn part_2_example_01_1212() {
                 let input = "1212";
                 let expected = 6;
-                let to_check = aoc_day01_part_two(input);
+                let to_check = aoc_day01_part_2(input);
 
                 assert_eq!(expected, to_check);
             }
 
             #[test]
-            fn example_02_1221() {
+            fn part_2_example_02_1221() {
                 let input = "1221";
                 let expected = 0;
-                let to_check = aoc_day01_part_two(input);
+                let to_check = aoc_day01_part_2(input);
 
                 assert_eq!(expected, to_check);
             }
 
             #[test]
-            fn example_03_123425() {
+            fn part_2_example_03_123425() {
                 let input = "123425";
                 let expected = 4;
-                let to_check = aoc_day01_part_two(input);
+                let to_check = aoc_day01_part_2(input);
 
                 assert_eq!(expected, to_check);
             }
 
             #[test]
-            fn example_04_123123() {
+            fn part_2_example_04_123123() {
                 let input = "123123";
                 let expected = 12;
-                let to_check = aoc_day01_part_two(input);
+                let to_check = aoc_day01_part_2(input);
 
                 assert_eq!(expected, to_check);
             }
 
             #[test]
-            fn example_05_12131415() {
+            fn part_2_example_05_12131415() {
                 let input = "12131415";
                 let expected = 4;
-                let to_check = aoc_day01_part_two(input);
+                let to_check = aoc_day01_part_2(input);
 
                 assert_eq!(expected, to_check);
             }
 
             #[test]
-            fn solution() {
-                const PUZZLE_INPUT: &'static str = include_str!("../day01_input.txt");
+            fn part_1_solution() {
+                const PUZZLE_INPUT: &'static str = include_str!("../input");
+                let expected = 1177;
+                let to_check = aoc_day01_part_1(PUZZLE_INPUT);
+
+                assert_eq!(expected, to_check);
+            }
+
+            #[test]
+            fn part_2_solution() {
+                const PUZZLE_INPUT: &'static str = include_str!("../input");
                 let expected = 1060;
-                let to_check = aoc_day01_part_two(PUZZLE_INPUT);
+                let to_check = aoc_day01_part_2(PUZZLE_INPUT);
 
                 assert_eq!(expected, to_check);
             }
